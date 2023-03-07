aad9d75672707c0e2eeb143d36c6a401ec2dc688
DenysProg <denysnikolaichuk@gmail.com>
2023-03-07 15:53:34 +0200

    Initial Commit

---
+++ UnSplashApp/UnSplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSplashAppApp.swift
+//  UnSplashApp
+//
+//  Created by Denys Nikolaichuk on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnSplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
